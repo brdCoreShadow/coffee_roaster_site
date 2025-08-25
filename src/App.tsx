6d57dfee8f882038a9952a87be681c6d0a2a8064
Oleksandr-Berd <alex.berd86@gmail.com>
2025-08-25 08:52:34 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as SC from "./AppStyled"
+
+const App:React.FC = () => {
+
+  return (
+   <SC.AppStyled>
+
+   </SC.AppStyled>
+  )
+}
+
+export default App
---
+++ src/AppStyled.ts
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const AppStyled = styled.div`
+min-width: 768px;
+
+@media (min-width: 1280px){
+    max-width: 1280px;
+
+    margin-left: auto;
+    margin-right: auto;
+}
+`
